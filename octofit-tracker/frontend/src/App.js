2f7c5e0767f9ab1fc1a9e35a4c69b66c59f2babc
William Ricardo Isidorio <william.isidorio@alumni.usp.br>
2025-04-08 04:39:22 +0000

    Enhanced OctoFit Tracker styling with updated App.css, logo, and favicon

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -14,6 +14,9 @@ function App() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">OctoFit Tracker</Link>
+            <div className="logo">
+              <img src="/octofitapp-small.png" alt="OctoFit Logo" />
+            </div>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
